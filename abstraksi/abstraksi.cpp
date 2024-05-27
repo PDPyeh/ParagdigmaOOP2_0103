35f1fa163650caec7aaea47d6f330215c4058ebf
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-27 10:34:28 +0700

    fungsi void di class abstraksiklas

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -4,7 +4,17 @@
 using namespace std;
 
 class AbstraksiKlas {
-
+private:
+	string x, y;
+public:
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	void display() {
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
+	}
 };
 
 int main()
