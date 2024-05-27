d7667da73089285de2c3df9b78eb0f19908d277b
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-27 10:17:37 +0700

    membuat integer main

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -19,7 +19,19 @@ public:
 
 int main()
 {
-   
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "lampu Teras Rumah");
+	lampuRumah.setSaklarNo(1, "lampu Ruang Tamu");
+	lampuRumah.setSaklarNo(2, "lampu Kamar Tidur");
+	lampuRumah.setSaklarNo(3, "lampu Dapur");
+
+	cout << lampuRumah.getSakLarNo(0) << endl;
+	cout << lampuRumah.getSakLarNo(1) << endl;
+	cout << lampuRumah.getSakLarNo(2) << endl;
+	cout << lampuRumah.getSakLarNo(3) << endl;
+
+	return 0;
 }
 
 
