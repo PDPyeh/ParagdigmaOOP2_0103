8c6e5c7b3ca1d908530e113df24c3319a9e2d49c
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-27 10:28:07 +0700

    integer main

--- NoOverriding/NoOverriding.cpp
+++ NoOverriding/NoOverriding.cpp
@@ -6,11 +6,21 @@ using namespace std;
 class baseClass {
 public:
 	virtual void perkenalan() {
-		cout
+		cout << "Hallo saya Function dar Base Class";
+	}
+};
+
+class derivedClass :public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
 	}
 };
 
 int main()
 {
-   
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
 }
