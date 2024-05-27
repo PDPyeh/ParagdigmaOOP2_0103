a2f86be02705af6869df99414ddd811eb6bf5a3d
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-27 10:42:04 +0700

    membuat class pekerja

--- Multiwaris/Multiwaris.cpp
+++ Multiwaris/Multiwaris.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 class orang {
+public:
+    int umur;
 
+    orang(int pUmur) :
+        umur(pUmur) {
+        cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+    }
+};
+class pekerja {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur) {
+        cout << "pekerja dibuat\n" << endl;
+    }
 };
 
 int main()
